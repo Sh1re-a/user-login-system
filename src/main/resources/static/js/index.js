f592d322f139b1dbc224cdeae350af182b8502be
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-03-24 00:38:34 +0100

    Problem med att få menyn att fade in

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,19 +1,26 @@
 var delayInMilliseconds = 5000; //1 second
 
 setTimeout(function() {
-  fadeOutEffect(
+  fadeOutEffect("animated-title"
   )
 }, delayInMilliseconds);
 
-function fadeOutEffect() {
-    var s = document.getElementById('animated-title').style;
+function fadeOutEffect(id) {
+    var s = document.getElementById(id).style;
     s.opacity = 1;
-    (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
-    var s2 = document.getElementById('animated-title')
-    
+    (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,30)})();  
 }
+
+function fadeInEffect(id) {
+    var div = document.getElementById(id).style;
+    div.opacity= 0;
+    (function fade(){(s.opacity+=.1)<0?s.display="none":setTimeout(fade,30)})();
+}
+
 
 setTimeout(function() {
     var s2 = document.getElementById('animated-title')
     s2.remove()
   }, 7000);
+
+fadeInEffect("whiteBackground")
