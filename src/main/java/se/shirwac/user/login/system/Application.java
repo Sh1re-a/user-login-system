005fb41e0b31634a20ad99a8aed7dcffe36f2d04
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-03-31 07:22:12 +0200

    Created webTest

--- src/main/java/se/shirwac/user/login/system/Application.java
+++ src/main/java/se/shirwac/user/login/system/Application.java
@@ -2,12 +2,16 @@ package se.shirwac.user.login.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.shirwac.user.login.system.webtest.WebTest;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		WebTest x = new WebTest();
+		x.runWebTest();
 	}
 
 }
